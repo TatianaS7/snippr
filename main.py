67566ef8f22a99c5f5900281578854b618f3c429
Tatiana <tstwrt7@gmail.com>
2024-04-19 13:14:11 -0400

    Encrypted seed data code before saving to db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from seed_data import seed_snippets
 from database import snippet_data
+from snippet import Fernet, f
 
 next_id = 1
 
@@ -8,6 +9,6 @@ def seedDataToDB():
     for snippet in seed_snippets:
         snippet_data[next_id] = {
             'language': snippet['language'],
-            'code': snippet['code']
+            'code': f.encrypt(snippet['code'].encode()).decode()
         }
         next_id += 1
